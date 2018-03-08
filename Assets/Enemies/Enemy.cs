c459f46becafa9c18376a6bc593635976ef36cbd
ConnorWin <cwinnest@gmail.com>
2018-03-07 20:36:56 -0500

    interface character

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour, IDamageable {
 
     [SerializeField] float maxHealthPoints = 100f;
-    [SerializeField] float attackRadius = 100f;
+    [SerializeField] float attackRadius = 5f;
+    [SerializeField] float moveRadius = 5f;
 
     float currentHealthPoints = 100f;
     AICharacterControl aiCharacterControl = null;
@@ -20,6 +21,11 @@ public class Enemy : MonoBehaviour {
         }
     }
 
+    public void TakeDamage(float damage)
+    {
+        currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+    }
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
@@ -31,11 +37,27 @@ public class Enemy : MonoBehaviour {
         float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
         if(distanceToPlayer <= attackRadius)
         {
+            print(gameObject.name + " attacking player");
+        }
+
+        if (distanceToPlayer <= moveRadius)
+        {
             aiCharacterControl.SetTarget(player.transform);
         }
         else
         {
             aiCharacterControl.SetTarget(transform);
         }
+    }
+
+    void OnDrawGizmos()
+    {
+        //Draw attack sphere
+        Gizmos.color = new Color(255f, 0f, 0, .5f);
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+        //Draw move sphere
+        Gizmos.color = new Color(0f, 0f, 255f, .5f);
+        Gizmos.DrawWireSphere(transform.position, moveRadius);
     }
 }
